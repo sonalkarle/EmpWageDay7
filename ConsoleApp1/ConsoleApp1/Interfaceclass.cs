ae846df4212869fa990b592698b211e56720dc3f
sonal <sonalkarle01@gmail.com>
2021-02-17 14:49:51 +0530

    Empwage computation for multiple companies using Interface approch

---
+++ ConsoleApp1/ConsoleApp1/Interfaceclass.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeWage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageCsharp
+{
+    interface InterfaceClass
+    {
+        public void AddCompany(string companyName, int wagePerHour, int maxWorkingDays, int maxWorkingHours);
+        public int ComputeMonthlyWage(CompanyEmpWage company);
+        public void DisplayCompanyWages();
+    }
+}
